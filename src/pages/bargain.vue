376d6e33d6510641a12ea276e0e4a0d3cdf9de02
zhouhannan <zhouhannan@163.com>
2018-08-02 18:25:17 +0800

    ui变更

--- src/pages/bargain.vue
+++ src/pages/bargain.vue
@@ -3,18 +3,19 @@
     <div class="bargainInfo">
       <div class="productDetail">
           <h1>小米4AL50M5-AD50英寸4k智能电视wifi网络平板液晶电视</h1>
-          <h2>砍后价:<i>￥1677</i></h2>
-          <h3>京东价:<i>￥7899</i></h3>
+          <!-- <h2>砍后价:<i>￥1677</i></h2>
+          <h3>京东价:<i>￥7899</i></h3> -->
+          <div class="productImg">
+            <div class="progress" @click="testClick">
+              <div class="progressBasic"></div>
+              <div class="progressLeftBasic" v-if="rotateAngle < 180"></div>
+              <div class="progressLeft" :style="{transform:'rotateZ('+ -(360 - rotateAngle) +'deg)'}"></div>
+              <div class="progressRight" v-if="rotateAngle>=180"></div>
+              <img src="https://img11.360buyimg.com/n1/jfs/t19138/240/937744554/579343/7fe7bb22/5ab240f6N6ad80f56.jpg">
+            </div>
+          </div>
       </div>
-      <div class="productImg">
-        <div class="progress" @click="testClick">
-          <div class="progressBasic"></div>
-          <div class="progressLeftBasic" v-if="rotateAngle < 180"></div>
-          <div class="progressLeft" :style="{transform:'rotateZ('+ -(360 - rotateAngle) +'deg)'}"></div>
-          <div class="progressRight" v-if="rotateAngle>=180"></div>
-        </div>
-        <!-- <img src="https://img11.360buyimg.com/n1/jfs/t19138/240/937744554/579343/7fe7bb22/5ab240f6N6ad80f56.jpg"> -->
-      </div>
+
     </div>
   </div>
 </template>
@@ -38,12 +39,14 @@ export default {
   created() {},
   mounted() {
     setTimeout(() => {
-      setInterval(() => {
+      let percentTimer = setInterval(() => {
         // 0.6是通过接口下发的拼团百分比计算
-        if (this.bargainPercent <= 1) {
-          this.bargainPercent += 0.004
+        if (this.bargainPercent <= 0.6) {
+          this.bargainPercent += 0.01
+        } else {
+          clearInterval(percentTimer)
         }
-      }, 10)
+      }, 20)
     }, 10)
   }
 }
@@ -52,23 +55,32 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang = 'less'>
 .bargainInfo {
-  width: 100%;
+  width: 750px;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  background-color: #ccc;
+  background-color: #f7cea7;
 
   .productDetail {
-    width: 500px;
-    padding: 0 125px;
-    margin-bottom: 30px;
+    width: 646px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding-bottom: 10px;
+    background-color: #fff;
+    border-radius: 20px;
     > h1 {
-      width: 100%;
-      font-size: 32px;
-      line-height: 32px;
-      margin: 20px 0;
+      width: 532px;
+      font-size: 28px;
+      letter-spacing: -0.42px;
+      color: #2e2d2d;
+      line-height: 40px;
+      text-align: center;
+      overflow: hidden;
+      margin-top: 46px;
+      margin-bottom: 34px;
     }
-    > h2 {
+    /* > h2 {
       min-width: 200px;
       height: 40px;
       line-height: 40px;
@@ -86,48 +98,51 @@ export default {
       font-size: 20px;
       color: #999;
       margin: 0 auto;
-    }
-  }
-  .productImg {
-    .progress {
-      width: 250px;
-      height: 250px;
-      background-color: #fff;
-      position: relative;
-      .progressLeft,
-      .progressBasic,
-      .progressRight,
-      .progressLeftBasic {
-        width: 250px;
-        height: 250px;
+    } */
+    .productImg {
+      .progress {
+        width: 420px;
+        height: 420px;
+        /* background-color: #fff; */
+        position: relative;
+        .progressLeft,
+        .progressBasic,
+        .progressRight,
+        .progressLeftBasic {
+          width: 420px;
+          height: 420px;
+          border-radius: 50%;
+          box-sizing: border-box;
+          position: absolute;
+          left: 0;
+          top: 0;
+        }
+        .progressBasic {
+          border: 15px solid #fef7ec;
+        }
+        .progressLeftBasic {
+          border: 15px solid #fef7ec;
+          clip: rect(0px, 210px, 420px, 0px);
+          z-index: 1;
+        }
+        .progressLeft {
+          border: 15px solid skyblue;
+          clip: rect(0px, 210px, 420px, 0px);
+          z-index: 0;
+        }
+        .progressRight {
+          border: 15px solid skyblue;
+          clip: rect(0, 420px, 420px, 210px);
+          /* display: none; */
+        }
+      }
+      img {
+        width: 354px;
+        height: 354px;
         border-radius: 50%;
-        box-sizing: border-box;
-        position: absolute;
-        left: 0;
-        top: 0;
+        margin: 0 auto;
+        margin-top: 28px;
       }
-      .progressBasic {
-        border: 10px solid #666;
-      }
-      .progressLeftBasic {
-        border: 10px solid #666;
-        clip: rect(0px, 125px, 250px, 0px);
-        z-index: 1;
-      }
-      .progressLeft {
-        border: 10px solid skyblue;
-        clip: rect(0px, 125px, 250px, 0px);
-        z-index: 0;
-      }
-      .progressRight {
-        border: 10px solid skyblue;
-        clip: rect(0, 250px, 250px, 125px);
-        /* display: none; */
-      }
-    }
-    img {
-      width: 200px;
-      height: 200px;
     }
   }
 }
